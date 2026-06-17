d20a3bc759afc5c4a42b5011e37f4f037d239dd9
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-17 22:11:07 +0545

    Add files via upload

---
+++ Day 02/03_Operator.py
@@ -0,0 +1,46 @@
+# Arithmetic operators
+
+a=45
+b=89
+c=a+b
+print(c)
+
+# Assigment operators
+a=4-3
+print(a)
+b=6*1
+b+=2 #Increment the value of b by 2
+print(b)
+
+# Comparison operators
+a=5
+b=6
+print(a==b) #Equal to
+print(a!=b) #Not equal to
+print(a>b) #Greater than
+print(a<b) #Less than
+print(a>=b) #Greater than or equal to
+print(a<=b) #Less than or equal to
+
+# Logical operators
+a=5
+b=6
+print(a>3 and b>5) #Logical AND
+print(a>3 or b>5) #Logical OR
+print(not(a>3 and b>5)) #Logical NOT
+
+# truth table for AND operator
+print("True and True is", True and True) 
+print("True and False is", True and False)
+print("False and True is", False and True) 
+print("False and False is", False and False) 
+
+# truth table for OR operator
+print("True or True is", True or True) 
+print("True or False is", True or False) 
+print("False or True is", False or True)
+print("False or False is", False or False) 
+
+#truth table for NOT operator
+print("not True is", not True)
+print("not False is", not False)
