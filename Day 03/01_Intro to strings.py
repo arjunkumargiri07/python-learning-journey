2793829947e868a2eefcadce069b328b96ef82de
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-20 22:05:46 +0545

    Add files via upload

---
+++ Day 03/01_Intro to strings.py
@@ -0,0 +1,14 @@
+name = "Alice"
+print(name)
+
+nameshort = name[0:3] # start from index 0 and end at index 3 (not including index 3) 
+print(nameshort)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[-1])
+print(name[-2])
+
+nameshort2 = len(name)
+print(nameshort2)
