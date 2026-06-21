12cfe636dd395afaec09c17d1cb648c1078e74cf
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-21 22:45:28 +0545

    Add files via upload

---
+++ Day 04/02_List_method.py
@@ -0,0 +1,22 @@
+friend= ["Apple","Arjun",7,True,54541.5]
+print(friend)
+
+friend.append("Hello")
+print(friend)
+
+l1=[5,3,9,7,1,11]
+l1.sort()
+print(l1)
+
+l1=[5,3,9,7,1,11]
+l1.reverse()
+print(l1)
+
+l1=[5,3,9,7,1,11]
+l1.insert(2, 54) #insert 54 such that its in index no 2
+print(l1)
+
+l1=[5,3,9,7,1,11]
+l1.pop(3)
+print(l1.pop(2))
+print(l1)
