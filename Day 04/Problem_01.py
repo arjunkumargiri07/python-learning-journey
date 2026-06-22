8e17e812bf17b6ca369d999fc2f0a0f6ad4e0e9e
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-22 22:21:36 +0545

    Add files via upload

---
+++ Day 04/Problem_01.py
@@ -0,0 +1,12 @@
+fruits=[]
+f1=input("Enter the name of the fruit: ")
+fruits.append(f1)
+f2=input("Enter the name of the fruit: ")
+fruits.append(f2)
+f3=input("Enter the name of the fruit: ")
+fruits.append(f3)
+f4=input("Enter the name of the fruit: ")
+fruits.append(f4)
+f5=input("Enter the name of the fruit: ")
+fruits.append(f5)
+print(fruits)
