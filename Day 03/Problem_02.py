61fa77a0943a12b1ba7a60867051b64590315f3d
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-22 22:26:18 +0545

    Add files via upload

---
+++ Day 03/Problem_02.py
@@ -0,0 +1,5 @@
+letter =""" Dear {name},
+        You are selected for the position of {position} in our company.
+        {Date}"""
+
+print(letter.format(name="Arjun", position="Software Engineer", Date="2024-06-15"))
