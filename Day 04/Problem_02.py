1534e7fdc417a3e05328aeccc0fc0ac74d8d4127
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-22 22:21:55 +0545

    Add files via upload

---
+++ Day 04/Problem_02.py
@@ -0,0 +1,16 @@
+
+marks=[]
+
+f1=int(input("Enter the name of the marks: "))
+marks.append(f1)
+f2=int(input("Enter the name of the marks: "))
+marks.append(f2)
+f3=int(input("Enter the name of the marks: "))
+marks.append(f3)
+f4=int(input("Enter the name of the marks: "))
+marks.append(f4)
+f5=int(input("Enter the name of the marks: "))
+marks.append(f5)
+marks.sort()
+
+print(marks)
