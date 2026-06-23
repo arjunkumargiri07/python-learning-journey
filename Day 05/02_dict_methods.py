848ce5bb9c598ae80a5dabf395b18ffd173b0a8b
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-23 22:22:38 +0545

    Add files via upload

---
+++ Day 05/02_dict_methods.py
@@ -0,0 +1,11 @@
+marks={
+    "Rohal": 90,
+    "Rohit": 80,
+    "Arjun":100
+}
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"Rohal": 95})
+print(marks)
+print(marks.get("Rohal"))
