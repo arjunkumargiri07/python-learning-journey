773ad3c0dfa0593ab9ac1a7a2753bc3ab99d6ba3
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-25 22:28:48 +0545

    Add files via upload

---
+++ Day 06/03_mulitple_if_statement.py
@@ -0,0 +1,24 @@
+a=int(input("Enter your age: "))
+
+# if statment no:1
+if(a%2 == 0):
+    print("a is even number.")
+
+ # End of If statement no:1
+
+# If statment no:2
+if(a>=18):
+    print("You are eligible to vote.")
+
+elif(a<0):
+    print("Invalid age entered.")
+
+elif(a==0):
+    print("You are entering zero.")
+
+else:
+    print("You are not eligible to vote.")
+print("Thank you for using the program.")
+
+    # End of if statement no:2
+print("This is the end of the program.")
