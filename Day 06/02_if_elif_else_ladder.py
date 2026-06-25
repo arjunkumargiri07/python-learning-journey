750ec00e04966d8c474a3c002942a21d88a1bf98
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-25 22:28:32 +0545

    Add files via upload

---
+++ Day 06/02_if_elif_else_ladder.py
@@ -0,0 +1,16 @@
+a=int(input("Enter your age: "))
+
+if(a>=18):
+    print("You are eligible to vote.")
+
+elif(a<0):
+    print("Invalid age entered.")
+
+elif(a==0):
+    print("You are entering zero.")
+
+else:
+    print("You are not eligible to vote.")
+print("Thank you for using the program.")
+
+print("This is the end of the program.")
