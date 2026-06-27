d60c374f15b43f78b66dc5d281eff795ae648b15
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-27 22:03:03 +0545

    Add files via upload

---
+++ Day 07/07_break_and_continous.py
@@ -0,0 +1,9 @@
+for i in range(1, 10):
+    if i == 5:
+        break  #exit the loop when i is 5
+    print(i)
+
+for i in range(1, 10):
+    if i == 5:
+        continue  #skip the rest of the loop when i is 5
+    print(i)
