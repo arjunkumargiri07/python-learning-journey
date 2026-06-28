5484e23b40ba2e5effe124aff2e2fa8d8d09f83d
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-28 22:12:05 +0545

    Add files via upload

---
+++ Day 08/01_function.py
@@ -0,0 +1,13 @@
+# Function definition
+def avg():
+    a= int(input("Enter your number:"))
+    b= int(input("Enter your number:"))
+    c= int(input("Enter your number:"))
+
+    average = (a + b + c) / 3
+    print("The average of the three numbers is:", average)
+avg() # Function call
+avg()
+avg()
+avg()
+avg()
