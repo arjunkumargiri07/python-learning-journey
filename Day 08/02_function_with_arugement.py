b160910e5e63dffb7d5c0e3fb7b7ccea47a77d61
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-28 22:15:38 +0545

    Add files via upload

---
+++ Day 08/02_function_with_arugement.py
@@ -0,0 +1,7 @@
+def goodDay(name, ending):
+    print("Good day, " + name + "!")
+    print("Have a great " + ending + "!")
+
+goodDay("Arjun", "Thank you")
+goodDay("Ravi", "Have a great day!")
+goodDay("Suresh", "Have a great day!")
