fc413b1912796ec33f0dfbf242cc6ef9e993f7a7
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-06-29 21:54:09 +0545

    Add files via upload

---
+++ Day 08/Problem_01.py
@@ -0,0 +1,11 @@
+def great(a,b,c):
+    if a > b and a > c:
+        return a
+    elif b > a and b > c:
+        return b
+    else:
+        return c
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+print("The greatest number is:", great(a,b,c))
