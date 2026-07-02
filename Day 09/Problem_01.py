21c64c8db7bb381bc207694cbb0ff17d3e8b8423
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-07-02 22:44:24 +0545

    Add files via upload

---
+++ Day 09/Problem_01.py
@@ -0,0 +1,6 @@
+f = open("poem.txt", "r")
+content = f.read()
+if ("twinkle" in content):
+             print("Twinkle is present in the poem")
+else:
+             print("Twinkle is not present in the poem")
