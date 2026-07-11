8e774b73a79abde77e6d323a32c5479eae434104
Arjun Kumar Giri <arjunkumargiri07@gmail.com>
2026-07-11 22:02:41 +0545

    Add files via upload

---
+++ Day 13/Problem_01.py
@@ -0,0 +1,7 @@
+name = input("Enter name :")
+marks = int(input("Enter the marks :"))
+phone = int(input("Enter phone number :"))
+
+s = "The name of the student is {}, his marks is {}, and his phone number is {}".format(name, marks, phone)
+
+print((s))
